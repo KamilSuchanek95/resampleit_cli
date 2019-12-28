682efaee4fe0ef259f96f7c2957afb930d6ddb62
ciarambola <kamisuc671@student.polsl.pl>
2019-12-28 13:02:07 +0100

    dodanie formy CLI za pomocą modułu docopt

--- program/resampleit.py
+++ program/resampleit.py
@@ -1,42 +1,51 @@
-#import argparse
-import numpy as np
-import scipy.signal as sig
+"""RESAMPLE IT CLI
+Usage:
+    resampleit.py
+    resampleit.py <target_frequency> <path>
+    resampleit.py -h|--help
+    resampleit.py -v|--version
+Options:
+    <path>  Full path to the record file with the extension .dat or .hea.
+    <target_frequency>  Target sampling rate.
+    -h --help  Show this screen.
+    -v --version  Show version.
+"""
+
+from docopt import docopt
+import numpy
 import wfdb
 from wfdb import processing
 from zenipy import file_selection
-import ipdb
 
-#parser = argparse.ArgumentParser(description='Process some integers.')
-#parser.add_argument('Path', metavar='p', type=str,
-#help='path to the file with the .dat or .hea signal')
+def resampleit(path, target_frequency):
 
-# arguments
-#args = parser.parse_args()
-#ipdb.set_trace()
+    # change file name
+    path = path.replace('.dat','')
+    path = path.replace('.hea','')
 
-file_name = file_selection(multiple=False, directory=False, save=False, confirm_overwrite=False, filename=None, title='', width=330, height=120, timeout=None)
-frequency = int(input("Enter target frequency: "))
+    # reading MIT file
+    record = wfdb.rdsamp(path)
 
-#ipdb.set_trace()
+    # create signal vector
+    signal = numpy.concatenate(record[0])
 
-# change file name
-file_name = file_name.replace('.dat','')
-file_name = file_name.replace('.hea','')
+    # resampling signal
+    signal_resample = processing.resample_sig(signal, record[1]['fs'], target_frequency)
 
-# reading MIT file
-record = wfdb.rdsamp(file_name)
+    wfdb.wrsamp(path.split('/')[-1] + '_resampled_to' + str(target_frequency),
+    fs = target_frequency, units = record[1]['units'],
+    fmt=["16"],
+    sig_name = record[1]['sig_name'],
+    p_signal = numpy.asarray(signal_resample[0].reshape(len(signal_resample[0]), 1)) )
 
-# create signal vector
-signal = np.concatenate(record[0])
+    print('Signal was resampled from %i [Hz] to %i [Hz]\n' %(record[1]['fs'], target_frequency))
+    print('The resulting files are located in the current folder\n')
 
-# resampling signal
-signal_resample = processing.resample_sig(signal, record[1]['fs'], frequency)
-
-wfdb.wrsamp(file_name.split('/')[-1] + '_resampled_' + str(frequency),
-fs = frequency, units = record[1]['units'],
-fmt=["16"],
-sig_name = record[1]['sig_name'],
-p_signal = np.asarray(signal_resample[0].reshape(len(signal_resample[0]), 1)) )
-
-print('Signal was resampled from %i [Hz] to %i [Hz]\n' %(record[1]['fs'], frequency))
-print('The resulting files are located in the current folder\n')
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version=' 1.0')
+    if arguments['<path>'] and arguments['<target_frequency>']:
+        resampleit(arguments['<path>'], arguments['<target_frequency>'])
+    else:
+        path = file_selection(multiple=False, directory=False, save=False, confirm_overwrite=False, filename=None, title='', width=330, height=120, timeout=None)
+        target_frequency = int(input("Enter target frequency: "))
+        resampleit(path, target_frequency)
